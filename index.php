5d2df155bd72d41b16e628d8a12bc29254f22fee
robin <robin@Robin-Workstation>
2017-01-05 17:32:25 +0100

    first

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Pendu</title>
+        <link rel="stylesheet" type="text/css" href="resources/css/main.css">
+        <script src="resources/js/jquery-3.1.1.min.js"></script>
+    </head>
+    <body>
+        
+    </body>
+</html>
